d3ca3440a8251ab61ed2ceec427c8e8253c8b977
Elad Keyshawn <elad.keyshawn@gmail.com>
2017-01-04 14:20:44 +0200

    added db_manager

---
+++ scraper/db_manager.py
@@ -0,0 +1,32 @@
+from pymongo import MongoClient
+import pymongo
+
+
+class DbManager:
+    def __init__(self):
+        self.collections = {}
+
+        try:
+            self.client = MongoClient('localhost', 27017, serverSelectionTimeoutMS=1)
+            self.client.server_info()
+            self.db = self.client['test']
+
+        except pymongo.errors.ServerSelectionTimeoutError as err:
+            print "problem connecting to server"
+            print err
+
+    def get_authorities(self):
+        return self.db.authorities
+
+    def get_gov_calls(self):
+        return self.db.gov_calls
+
+    def get_collections(self):
+        return self.db.test_collection
+
+    def insert_gov_calls(self, govcall_obj):
+
+
+manager = DbManager()
+
+print manager.get_authorities()
